eaff74b1c453b946231abc7ec140a71ad5b44995
axandregorgol <71868220+axandregorgol@users.noreply.github.com>
2021-10-18 17:35:50 +0200

    Update Tournoi.java

--- src/projettennis/Tournoi.java
+++ src/projettennis/Tournoi.java
@@ -16,44 +16,6 @@ import java.io.IOException;
  */
 public class Tournoi {
     
-    public static void ListeJoueur(int NbrJoueur, String AdrFile) throws FileNotFoundException, IOException{
-      
     
-    File file = new File(AdrFile);
-    FileReader fr = new FileReader(file);
-    BufferedReader br = new BufferedReader(fr);
-    StringBuffer sb = new StringBuffer();
-    String line;
-    while((line = br.readLine()) != null){
-        
-        sb.append(line);
-        sb.append("\n");
-        
-    }
-    fr.close();
-        System.out.println(sb.toString());
-   
-        Joueur[] Player = new Joueur[NbrJoueur];       
-        
-        for (int i=1; i<=NbrJoueur; i++){
-             Player[i-1] = new Joueur();
-             
-             
-             /*
-             Player[i-1].bras=
-             Player[i-1].entraineur=
-             Player[i-1].nomNaissance=
-             Player[i-1].sponsor=
-             Player[i-1].nationalite=
-             Player[i-1].nomCourant=
-             Player[i-1].poids=
-             Player[i-1].taille=    
-             Player[i-1].prenom=
-             Player[i-1].classement=i-1;
-             Player[i-1].surnom=
-*/
-        }
-        
-    }
     
 }
