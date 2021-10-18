d67b83b0d22d225a6aa832d8b0f940927fccfb30
axandregorgol <71868220+axandregorgol@users.noreply.github.com>
2021-10-18 17:34:25 +0200

    Update ProjetTennis.java

--- src/projettennis/ProjetTennis.java
+++ src/projettennis/ProjetTennis.java
@@ -4,27 +4,28 @@
  * and open the template in the editor.
  */
 package projettennis;
-import java.io.*;
-import projettennis.Personne;
-import projettennis.Spectateur;
-import projettennis.Joueur;
-import projettennis.Arbitre;
-import projettennis.Tournoi;
+import java.io.IOException;
+
+
 /**
  *
- * @author iPraz
+ * @author axand
  */
 public class ProjetTennis {
     
 
    
     public static void main(String[] args) throws IOException {
-       int NbrJoueur = 128;
-       
-       String AdrFile="info-joueur.txt";
-       
-       Tournoi.ListeJoueur(NbrJoueur, AdrFile);
         
+       int NbrJoueur = 128;
+       int NbrArbitre = 10;
+       
+       String AdrFileJ="info-joueur.txt";
+       String AdrFileA="info-arbitre.txt";
+       
+       Joueur.ListeJoueur(NbrJoueur, AdrFileJ );
+       Arbitre.ListeArbitre(NbrArbitre, AdrFileA);
+       
        Arbitre Axandre = new Arbitre();
        Axandre.nomNaissance = "gorgol";
        Axandre.faute();
